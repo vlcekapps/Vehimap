6a019fddff01e6515d62f06a6faacdd1856e3144
Pavel Vlcek <vlcekpavel93@gmail.com>
2026-04-02 09:04:03 +0200

    Improve Avalonia startup focus for screen readers

--- dotnet/src/Vehimap.Desktop/Views/MainWindow.axaml.cs
+++ dotnet/src/Vehimap.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Threading;
 using Avalonia.Markup.Xaml;
 using Vehimap.Desktop.ViewModels;
@@ -8,17 +9,25 @@ namespace Vehimap.Desktop.Views;
 public partial class MainWindow : Window
 {
     private MainWindowViewModel? _viewModel;
+    private bool _initialFocusCompleted;
+    private bool _initialFocusScheduled;
 
     public MainWindow()
     {
         AvaloniaXamlLoader.Load(this);
         Opened += OnOpened;
+        Activated += OnActivated;
         DataContextChanged += OnDataContextChanged;
     }
 
     private void OnOpened(object? sender, EventArgs e)
     {
-        RequestFocus(DesktopFocusTarget.VehicleList);
+        ScheduleInitialFocus();
+    }
+
+    private void OnActivated(object? sender, EventArgs e)
+    {
+        ScheduleInitialFocus();
     }
 
     private void OnDataContextChanged(object? sender, EventArgs e)
@@ -40,13 +49,74 @@ public partial class MainWindow : Window
         RequestFocus(target);
     }
 
+    private void ScheduleInitialFocus()
+    {
+        if (_initialFocusCompleted || _initialFocusScheduled)
+        {
+            return;
+        }
+
+        _initialFocusScheduled = true;
+        Dispatcher.UIThread.Post(() =>
+        {
+            Focus();
+            DispatcherTimer.RunOnce(() =>
+            {
+                _initialFocusScheduled = false;
+                _initialFocusCompleted = TryFocusTarget(DesktopFocusTarget.VehicleList);
+            }, TimeSpan.FromMilliseconds(140));
+        }, DispatcherPriority.Loaded);
+    }
+
     private void RequestFocus(DesktopFocusTarget target)
     {
         Dispatcher.UIThread.Post(() =>
         {
-            var control = ResolveFocusTarget(target);
-            control?.Focus();
+            if (!TryFocusTarget(target))
+            {
+                Dispatcher.UIThread.Post(() => TryFocusTarget(target), DispatcherPriority.Input);
+            }
         }, DispatcherPriority.Background);
+    }
+
+    private bool TryFocusTarget(DesktopFocusTarget target)
+    {
+        var control = ResolveFocusTarget(target);
+        if (control is null)
+        {
+            return false;
+        }
+
+        if (control is ListBox listBox)
+        {
+            return FocusListBox(listBox);
+        }
+
+        return control.Focus(NavigationMethod.Unspecified, KeyModifiers.None);
+    }
+
+    private static bool FocusListBox(ListBox listBox)
+    {
+        if (listBox.ItemCount <= 0)
+        {
+            return listBox.Focus(NavigationMethod.Unspecified, KeyModifiers.None);
+        }
+
+        if (listBox.SelectedItem is null && listBox.ItemCount > 0)
+        {
+            listBox.SelectedIndex = 0;
+        }
+
+        if (listBox.SelectedItem is not null)
+        {
+            listBox.ScrollIntoView(listBox.SelectedItem);
+            if (listBox.ContainerFromItem(listBox.SelectedItem) is Control itemContainer)
+            {
+                return itemContainer.Focus(NavigationMethod.Unspecified, KeyModifiers.None);
+            }
+        }
+
+        return listBox.Focus(NavigationMethod.Unspecified, KeyModifiers.None);
     }
 
     private Control? ResolveFocusTarget(DesktopFocusTarget target)
