8b49d52787829836c5e212396e15a9ff3dda8702
pavelvlcek <vlcekpavel93@gmail.com>
2026-07-01 09:26:48 +0200

    test(dotnet): stabilize unit test culture

---
+++ dotnet/tests/Vehimap.Tests.Unit/TestCultureInitializer.cs
@@ -0,0 +1,25 @@
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Vehimap.Application.Models;
+using Vehimap.Application.Services;
+using Vehimap.Desktop.Localization;
+
+namespace Vehimap.Tests.Unit;
+
+internal static class TestCultureInitializer
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        var culture = CultureInfo.GetCultureInfo(AppCultureService.CzechLanguage);
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+        CultureInfo.CurrentCulture = culture;
+        CultureInfo.CurrentUICulture = culture;
+
+        DesktopLocalization.Configure(new AppCulturePreferences(
+            AppCultureService.CzechLanguage,
+            AppCultureService.NoSeparator,
+            AppCultureService.CommaSeparator));
+    }
+}
